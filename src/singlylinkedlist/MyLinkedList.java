298e2d7cf6a6976a0ddc067ec4f46d86e00d3130
divaibhav <divaibhav@gmail.com>
2020-01-29 03:47:54 +0530

    defined insert method, to insert a node in the list, this method will add node at the end of linked list

--- src/singlylinkedlist/MyLinkedList.java
+++ src/singlylinkedlist/MyLinkedList.java
@@ -22,4 +22,24 @@ public class MyLinkedList {
         return head;
     }
 
+    //a method to insert a node in the list
+    // this method will add node at the end of linked list
+    public void insert(int data) {
+        // creating node
+        Node node = new Node(data);
+        // checking if list is empty, if the list is empty we will insert node at head
+        if (head == null) {
+            head = node;
+        } else {
+            // assigning head to a temp node, to travers the list and insert node at the end
+            Node temp = head;
+            // a loop to reach end of linked list
+            while (temp.getNext() != null) {
+                // if the next node is available, updating temp to refer next node
+                temp = temp.getNext();
+            }
+            // now temp is referring to last node of the list
+            temp.setNext(node);
+        }
+    }
 }
